b9af620ef178617cb32a08744af7a1a29d4e0473
SBastos800 <soniavsbastos@gmail.com>
2020-04-23 15:08:59 +0100

    calculator with simple operators

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # js-calculator
+
+## BUILD A JS CALCULATOR
+
+* Create a calculator with buttons for the numbers 0-9 and +, -, /, * and =.
+* Show the user the result of the calculator in the HTML when = is pressed.
+* Show the numbers that the user inputs on the screen as they are pressed (like the one in Google).
---
+++ app.js
@@ -0,0 +1,61 @@
+const answer = document.querySelector('.answer');
+const buttons = document.querySelectorAll('.button');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalButton = document.getElementById('equal');
+const reset = document.querySelector('.clear');
+
+let secondNumber;
+let firstNumber;
+let operator;
+
+reset.addEventListener('click', () => {
+    answer.innerHTML = "";
+});
+
+numberButtons.forEach((button) => {
+   button.addEventListener('click', (event) => {
+       if (firstNumber) {
+         secondNumber = (event.target.value)
+       } else {
+         firstNumber = (event.target.value);
+    }
+   })
+});
+
+numberButtons.forEach((n) => {
+    n.addEventListener('click', (event) => {
+       answer.innerHTML = event.target.value
+   } )
+});
+
+operatorButtons.forEach((o) => {
+   o.addEventListener('click', (event) => {
+       answer.innerHTML = event.target.value
+   })
+});
+
+operatorButtons.forEach((button) => {
+button.addEventListener('click', (event) => {
+    operator = (event.target.value);
+   })
+});
+
+equalButton.addEventListener('click', () => {
+    answer.innerHTML = calculate(firstNumber, operator, secondNumber)
+});
+
+const calculate = (firstNumber, operator, secondNumber) => {
+const firstFloat = parseFloat(firstNumber);
+const secondFloat = parseFloat(secondNumber);
+switch (operator) {
+    case "+":
+        return firstFloat + secondFloat;
+    case "-":
+        return firstFloat - secondFloat;
+    case "/":
+        return firstFloat / secondFloat;
+    case "*":
+        return firstFloat * secondFloat;
+}
+}
